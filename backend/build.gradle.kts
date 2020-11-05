85a803a4264dd7a68463b1219f944a5e092b1f54
1lio <svoefremov@yandex.ru>
2020-11-05 15:27:09 +0300

    Remove unused dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,19 +10,12 @@ application {
 dependencies {
 
     implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
-    implementation(Config.Libs.Kotlin.serialization)
 
     implementation(Config.Libs.Ktor.serverNetty)
-    implementation(Config.Libs.Ktor.serverCore)
-    implementation(Config.Libs.Ktor.serverHostCommon)
-
-    implementation(Config.Libs.Ktor.locations)
-    implementation(Config.Libs.Ktor.sessions)
     implementation(Config.Libs.Ktor.auth)
-    implementation(Config.Libs.Ktor.authJWT)
     implementation(Config.Libs.Ktor.gson)
 
     implementation(Config.Libs.Ktor.logbackClassic)
-    implementation(Config.Libs.Ktor.tests)
+
+    testImplementation(Config.Libs.Ktor.serverTests)
 }
